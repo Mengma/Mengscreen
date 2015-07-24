ddc79f97aa40c60a2c21bfcdaf72ed9ec867f954
James Liu <jamesleu96@gmail.com>
2015-07-24 17:18:55 +0800

    Initial Commit

---
+++ Mengscreen/MengscreenView.h
@@ -0,0 +1,13 @@
+//
+//  MengscreenView.h
+//  Mengscreen
+//
+//  Created by James Liu on 15/7/24.
+//  Copyright (c) 2015年 mengma. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface MengscreenView : ScreenSaverView
+
+@end
---
+++ Mengscreen/MengscreenView.m
@@ -0,0 +1,52 @@
+//
+//  MengscreenView.m
+//  Mengscreen
+//
+//  Created by James Liu on 15/7/24.
+//  Copyright (c) 2015年 mengma. All rights reserved.
+//
+
+#import "MengscreenView.h"
+
+@implementation MengscreenView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
